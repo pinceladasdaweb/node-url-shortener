6594f1d9b56a21c77ea06d532da101d04a5364fe
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-05-29 19:16:36 -0300

    feat(*): created project structure

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const { app: server } = require('./app')
+const { APP_PORT } = require('./environment')
+
+server()
+  .then(app => {
+    app.listen(APP_PORT, '0.0.0.0')
+      .then(_ => {
+        process.on('SIGINT', () => {
+          app.close()
+          process.exit(0)
+        })
+          .on('SIGTERM', () => {
+            app.close()
+            process.exit(0)
+          })
+          .on('uncaughtException', err => {
+            console.error(err.stack)
+            process.exit(1)
+          })
+          .on('unhandledRejection', (reason, promise) => {
+            console.error(reason, `Unhandled rejection at Promise: ${promise}`)
+          })
+      })
+      .catch(err => {
+        console.log('Error starting server: ', err)
+        process.exit(1)
+      })
+  })
+  .catch(err => console.log(err))
---
+++ src/plugins/sentry.js
@@ -0,0 +1,17 @@
+const Sentry = require('@sentry/node')
+const fastifyPlugin = require('fastify-plugin')
+const { name, version } = require('../../package.json')
+const { NODE_ENV, SENTRY_DSN } = require('../environment')
+
+module.exports = fastifyPlugin((fastify, options, next) => {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: NODE_ENV,
+    release: `${name}@${version}`,
+    enabled: !!SENTRY_DSN
+  })
+
+  !!SENTRY_DSN && fastify.log.info('Sentry is loaded')
+
+  next()
+})
