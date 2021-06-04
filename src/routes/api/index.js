3ff6afde300750adcd16b3efd12f5b63bf9a9cec
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-06-04 15:37:49 -0300

    feat(*): added encode route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,4 +1,5 @@
 const apiRoutes = async (app, options) => {
+  app.register(require('./encode'), { prefix: 'encode' })
   app.get('/', async (request, reply) => {
     return {
       message: 'Node Url Shortener API is on fire'
