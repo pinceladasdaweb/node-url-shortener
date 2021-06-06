96152d46aae2b526189de97938f8cfec3bd16a67
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-06-06 09:40:58 -0300

    feat(*): added route schema

--- src/routes/api/hash/index.js
+++ src/routes/api/hash/index.js
@@ -1,7 +1,9 @@
+const { statsSchema } = require('./schemas')
 const { hashController } = require('../../../controllers')
 
 const hashRoutes = async (app, options) => {
   app.get('/:hash', { pg: { transact: true } }, hashController.redirect)
+  app.get('/:hash/stats', { schema: statsSchema, pg: { transact: true } }, hashController.stats)
 }
 
 module.exports = hashRoutes
